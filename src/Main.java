bcdd0d5193494c77b7227305304b8f457b38c09c
Harsh <harsh.devavanshi@autozone.com>
2025-08-24 08:01:52 +0530

    Binary Tree basic implementation

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Application Started Successfully");
+        Node root = new Node(10);
+        BinaryTree bt = new BinaryTree();
+        bt.root = root;
+        bt.insert(root,30);
+        bt.insert(root,40);
+        bt.insert(root,40);
+        bt.insert(root,440);
+        bt.insert(root,6);
+        bt.insert(root,20);
+        bt.insert(root,15);
 
-        System.out.println("SinglyLinkedList implementation");
-        SinglyLinkedList<Integer> singlyLinkedList = new SinglyLinkedList<>();
-        System.out.print("Values added in the singlyLinkedList are : ");
-        for(int val = 0; val < 10; val++, val++){
-            System.out.print(val+" ");
-            singlyLinkedList.insert(val);
-        }
+        bt.inOrderTraversal(root);
         System.out.println();
-        singlyLinkedList.traverse();
-        System.out.println(singlyLinkedList.search(55));
-        System.out.println(singlyLinkedList.search(4));
+        bt.preOrderTraversal(root);
+        System.out.println();
+        bt.postOrderTraversal(root);
 
-        singlyLinkedList.insertAt(99, 88);
-        System.out.println();
-        singlyLinkedList.traverse();
+    }
+}
+
+
+class BinaryTree<Integer>{
+    Node root;
+    public Node insert(Node root, int val){
+        Node new_Node = new Node(val);
+        if(root==null) return new_Node;
+        if(root.data>val) root.left = insert(root.left, val);
+        else root.right = insert(root.right, val);
+        return root;
+    }
+    public void inOrderTraversal(Node root){
+        if(root == null) return;
+        inOrderTraversal(root.left);
+        System.out.print(root.data+" ");
+        inOrderTraversal(root.right);
+    }
+
+    public void preOrderTraversal(Node root){
+        if(root == null) return;
+        System.out.print(root.data+" ");
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+    public void postOrderTraversal(Node root){
+        if(root == null) return;
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+        System.out.print(root.data+" ");
+    }
+}
+
+class Node<Integer>{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.left = null;
+        this.right = null;
+        this.data = data;
     }
 }
